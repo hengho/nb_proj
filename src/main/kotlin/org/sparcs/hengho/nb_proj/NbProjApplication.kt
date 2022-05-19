de2e324fc0ba36addd8bd7a5b2dd6cf5d9863752
hengho <gksghyou@gmail.com>
2022-05-19 22:56:34 +0900

    first commit

---
+++ src/main/kotlin/org/sparcs/hengho/nb_proj/NbProjApplication.kt
@@ -0,0 +1,11 @@
+package org.sparcs.hengho.nb_proj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NbProjApplication
+
+fun main(args: Array<String>) {
+    runApplication<NbProjApplication>(*args)
+}
---
+++ src/test/kotlin/org/sparcs/hengho/nb_proj/NbProjApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.sparcs.hengho.nb_proj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NbProjApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
